afecf3192a99bf6b05c155a9fd01d1a9269d3275
Tim Michielsen <tim.michiel1@gmail.com>
2025-02-18 20:33:32 +0100

    Init test

---
+++ src/dataset.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+# Creating a DataFrame
+
+data = {
+    "Name": ["Tim", "Wendy", "Ben", "Emma"],
+    "Age": [43, 36, 3, 0],
+    "City": ["Helmond", "Geldrop", "Veldhoven", "Veldhoven"],
+}
+
+pd.DataFrame(data)
+
+
+# def add(
+#     a,
+#     b,
+# ):
+#     return a + b
+
+
+# a = 10
+# b = 5
+
+# print(add(a, b))
